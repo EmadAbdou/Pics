964d195904ac9a77c68103ff4b17d2b1180d34fa
Emad Abdou <emad_abdou7@yahoo.com>
2019-02-02 12:09:59 +0200

    Updates

---
+++ precache-manifest.31bcc7c0b312b50c56326a69c5929fef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10239d91fd153dd50685",
+    "url": "/EmadAbdou/Pics/static/js/runtime~main.10239d91.js"
+  },
+  {
+    "revision": "2e3335e26ee42bc76557",
+    "url": "/EmadAbdou/Pics/static/js/main.2e3335e2.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/EmadAbdou/Pics/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "2e3335e26ee42bc76557",
+    "url": "/EmadAbdou/Pics/static/css/main.80faca60.chunk.css"
+  },
+  {
+    "revision": "6ca70ed6cd59689cdb95a5f16001eb1b",
+    "url": "/EmadAbdou/Pics/index.html"
+  }
+];
